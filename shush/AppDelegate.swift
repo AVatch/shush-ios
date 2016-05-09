00c44477b7f85e83b39c6ad79a88cfa74ba4c4d0
Adrian Vatchinsky <avatchinsky@outlook.com>
2016-05-09 10:43:33 -0400

    shush app

--- shush/AppDelegate.swift
+++ shush/AppDelegate.swift
@@ -12,10 +12,54 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var toggleDND = false
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        // ref: https://developer.apple.com/library/ios/documentation/NetworkingInternet/Conceptual/RemoteNotificationsPG/Chapters/IPhoneOSClientImp.html
+        
+        // create an action
+        let acceptAction = UIMutableUserNotificationAction()
+        // The identifier that you use internally to handle the action.
+        acceptAction.identifier = "TOGGLE_DND"
+        // The localized title of the action button.
+        acceptAction.title = "Do Not Disturb"
+        // Specifies whether the app must be in the foreground to perform the action.
+        acceptAction.activationMode = UIUserNotificationActivationMode.Background
+        acceptAction.authenticationRequired = true
+        acceptAction.destructive = false
+        
+        
+        // create a category
+        let inviteCategory = UIMutableUserNotificationCategory()
+        // Identifier to include in your push payload and local notification
+        inviteCategory.identifier = "BEACON_MONITORING"
+        // Set the actions to display
+        inviteCategory.setActions([acceptAction], forContext: UIUserNotificationActionContext.Default)
+        inviteCategory.setActions([acceptAction], forContext: UIUserNotificationActionContext.Minimal)
+        
+        // registering notifications and categories
+        let categories: Set = Set([inviteCategory])
+        
+        
+        application.registerUserNotificationSettings(
+            UIUserNotificationSettings(
+                forTypes: [UIUserNotificationType.Alert, UIUserNotificationType.Sound, UIUserNotificationType.Badge],
+                categories: categories
+            )
+        )
+        
+//        if(application.respondsToSelector(#selector(UIApplication.registerUserNotificationSettings(_:)))) {
+//            application.registerUserNotificationSettings(
+//                UIUserNotificationSettings(
+//                    forTypes: [UIUserNotificationType.Alert, UIUserNotificationType.Sound, UIUserNotificationType.Badge],
+//                    categories: categories
+//                )
+//            )
+//        }
+//        
+        
         return true
     }
 
@@ -35,12 +79,44 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        print("[applicationDidBecomeActive] toggleDND is")
+        print(toggleDND)
+        
+        if toggleDND {
+            toggleDND = false
+            UIApplication.sharedApplication().openURL(NSURL(string:"prefs:root=DO_NOT_DISTURB")!)
+        }
+        
     }
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
+    
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        print("got notfication")
+        print(notification.userInfo!["TOGGLE_DND"])
+        
+        toggleDND = notification.userInfo!["TOGGLE_DND"] as! Bool
+        
+    }
+    
 
+    func application(application: UIApplication, handleActionWithIdentifier identifier: String?, forLocalNotification notification: UILocalNotification, completionHandler: () -> Void) {
+        
+        print("handleActionWithIdentifier: \(identifier)")
+        
+//        print("The identifier is")
+//        print(identifier)
+//        
+//        if(identifier == "TOGGLE_DND"){
+//
+//        }
+        completionHandler()
+    }
+    
 
 }
 
--- shush/ViewController.swift
+++ shush/ViewController.swift
@@ -7,17 +7,151 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    // MARK: Properties
+    
+    var locationManager: CLLocationManager?
+    var lastProximity: CLProximity?
+    @IBOutlet weak var proximityLabel: UILabel!
+    @IBOutlet weak var regionLabel: UILabel!
+
+    
+    
+    // MARK: Notifications
+    
+    func sendLocalNotificationWithMessage(message: String!) {
+        let notification:UILocalNotification = UILocalNotification()
+        notification.alertBody = message
+        notification.category = "BEACON_MONITORING"
+        notification.userInfo = ["TOGGLE_DND": true]
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    
+    // MARK: Beacons
+    
+    func locationManager(manager: CLLocationManager, didEnterRegion region: CLRegion) {
+        // ENTER REGION
+        manager.startRangingBeaconsInRegion(region as! CLBeaconRegion)
+        manager.startUpdatingLocation()
+        
+        print("You entered the region")
+        regionLabel.text = "In quite region"
+        sendLocalNotificationWithMessage("You entered the region, enable do not disturb")
+    }
+    
+    func locationManager(manager: CLLocationManager, didExitRegion region: CLRegion) {
+        // EXIT REGION
+        manager.stopRangingBeaconsInRegion(region as! CLBeaconRegion)
+        manager.stopUpdatingLocation()
+        
+        print("You exited the region")
+        regionLabel.text = "Outside the quite region"
+        sendLocalNotificationWithMessage("You left the region, disable do not disturb")
+    }
+    
+    func locationManager(manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], inRegion region: CLBeaconRegion) {
+        // RANGING
+        
+//        print("didRangeBeacons()")
+        print("Ranging: " + (randomStringWithLength(10) as String))
+
+        var message:String = ""
+        
+        if(beacons.count > 0) {
+            let nearestBeacon:CLBeacon = beacons[0] as CLBeacon
+            
+            
+            // ensure we only send signal with new beacon
+            if(nearestBeacon.proximity == lastProximity ||
+                nearestBeacon.proximity == CLProximity.Unknown) {
+                return;
+            }
+            lastProximity = nearestBeacon.proximity;
+            
+            
+            
+            switch nearestBeacon.proximity {
+            case CLProximity.Far:
+                message = "You are far away from the beacon"
+            case CLProximity.Near:
+                message = "You are near the beacon"
+            case CLProximity.Immediate:
+                message = "You are in the immediate proximity of the beacon"
+            case CLProximity.Unknown:
+                return
+            }
+        } else {
+            message = "No beacons are nearby"
+        }
+        
+        proximityLabel.text = message
+        if( message == "No beacons are nearby"){
+            regionLabel.text = "Outside the quite region"
+        }else{
+            regionLabel.text = "In quite region"
+        }
+        
+        
+        
+//        print(message)
+//        sendLocalNotificationWithMessage(message)
+        
+    }
+    
+    
+    // MARK: viewDidLoad()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        // Define the region to monitor
+        let uuidString = "B9407F30-F5F8-466E-AFF9-25556B57FE6D"
+        let beaconIdentifier = "estimotes"
+        let beaconUUID:NSUUID = NSUUID(UUIDString: uuidString)!
+        let beaconRegion:CLBeaconRegion = CLBeaconRegion(proximityUUID: beaconUUID, identifier: beaconIdentifier)
+        
+        
+        // Setup the locationManager
+        locationManager = CLLocationManager()
+        if(locationManager!.respondsToSelector(#selector(CLLocationManager.requestAlwaysAuthorization))) {
+            locationManager!.requestAlwaysAuthorization()
+        }
+        locationManager!.delegate = self
+        locationManager!.pausesLocationUpdatesAutomatically = false
+        
+        // Start monitoring and ranging
+        locationManager!.startMonitoringForRegion(beaconRegion)
+        locationManager!.startRangingBeaconsInRegion(beaconRegion)
+        locationManager!.startUpdatingLocation()
+
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: Other
+    func randomStringWithLength (len : Int) -> NSString {
+        
+        let letters : NSString = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+        
+        var randomString : NSMutableString = NSMutableString(capacity: len)
+        
+        for (var i=0; i < len; i++){
+            var length = UInt32 (letters.length)
+            var rand = arc4random_uniform(length)
+            randomString.appendFormat("%C", letters.characterAtIndex(Int(rand)))
+        }
+        
+        return randomString
     }
 
 
